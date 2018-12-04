02dcccb47b7a310ed4f9cfd09c938373891acd84
Taesu Hyeon <qvil1127@gmail.com>
2018-12-05 01:13:18 +0900

    #1.8 Pretty URLS part One - Server Setup

--- components/PostLink.js
+++ components/PostLink.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default ({ title }) => (
-  <Link href={`/post?title=${title}`}>
+  <Link href={`/post?title=${title}`} as={`/post/${title}`}>
     <a>{title}</a>
   </Link>
 );
